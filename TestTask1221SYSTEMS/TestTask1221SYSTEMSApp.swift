b0ea4da970ef10e7f860e7988a1f95c33fb1f1b2
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-08-14 15:26:44 +0300

    Initial Commit

---
+++ TestTask1221SYSTEMS/TestTask1221SYSTEMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask1221SYSTEMSApp.swift
+//  TestTask1221SYSTEMS
+//
+//  Created by Валерия Дементьева on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTask1221SYSTEMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
